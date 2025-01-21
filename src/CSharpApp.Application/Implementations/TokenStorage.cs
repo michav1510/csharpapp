764cf0f84c9c365fc315768d84f2d116ee6af578
michav1510 <michav1510@gmail.com>
2025-01-21 21:17:11 +0200

    Add ITokenStorage interface and implementation in order to store the bearer tokwn

---
+++ src/CSharpApp.Application/Implementations/TokenStorage.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharpApp.Application.Implementations
+{
+    public class TokenStorage : ITokenStorage
+    {
+        private string? _token;
+        private DateTime? _expires;
+        
+        public string GetToken()
+        {
+            return _token;
+        }
+
+        public void SaveToken(string token)
+        {
+            _token = token;
+            _expires = DateTime.UtcNow.AddDays(20);
+        }
+
+        public bool IsTokenValid()
+        {
+            if(string.IsNullOrEmpty(_token))
+            {
+                return false;
+            }
+            DateTime now = DateTime.UtcNow;
+            if (now < _expires)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
