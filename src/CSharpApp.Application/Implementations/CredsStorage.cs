b45a83da4dc7872d290852d18c25c02d493f6e9a
michav1510 <michav1510@gmail.com>
2025-01-21 21:18:46 +0200

    Add interface and implementation of Credentials storage that stores the username and password for the bearer token

---
+++ src/CSharpApp.Application/Implementations/CredsStorage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CSharpApp.Application.Implementations
+{
+    public class CredsStorage : ICredsStorage
+    {
+        private readonly string _email;
+        private readonly string _password;
+        public CredsStorage(IConfiguration configuration)
+        {
+            _email = configuration["RestApiSettings:Username"];
+            _password = configuration["RestApiSettings:Password"];
+        }
+
+        public string GetEmail()
+        {
+            return _email;
+        }
+
+        public string GetPassword()
+        {
+            return _password;
+        }
+    }
+}
---
+++ src/CSharpApp.Core/Interfaces/ICredsStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpApp.Core.Interfaces
+{
+    public interface ICredsStorage
+    {
+        public string GetEmail();
+
+        public string GetPassword();
+    }
+}
